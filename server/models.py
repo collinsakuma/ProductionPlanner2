2670b9ed02ec5398a4c269bd80e459bf25720380
Collin Sakuma <collinmsakuma@gmail.com>
2024-08-21 15:18:03 -0700

    add operation tables and machine table relationship

--- server/models.py
+++ server/models.py
@@ -11,3 +11,14 @@ class Machine(db.Model, SerializerMixin):
     machine_name = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    operations = db.relationship('Operation', backref='machine')
+
+class Operation(db.MOdel, SerializerMixin):
+    __tablename__ = 'operations'
+
+    serialize_rules = ('-machine')
+
+    id = db.Column(db.Integer, primary_key=True)
+    operation_name = db.Column(db.String)
+    machine_id = db.Column(db.Integer, db.ForeignKey('machines.id'))
