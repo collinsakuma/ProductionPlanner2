bfda537e3f3077633f2a542863018a29fab0c1c2
Collin Sakuma <collinmsakuma@gmail.com>
2024-08-21 10:35:32 -0700

    added machine table, import db from config and add flask sql imports

--- server/models.py
+++ server/models.py
@@ -1,1 +1,13 @@
-# models
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+from config import db
+
+
+class Machine(db.Model, SerializerMixin):
+    __tablename__ = 'machines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    machine_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
