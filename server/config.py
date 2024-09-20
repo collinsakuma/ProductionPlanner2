9e41383b00a1aaeba8d8d4a27303a59ccfd9a888
Collin Sakuma <collinmsakuma@gmail.com>
2024-09-19 20:14:58 -0700

    update config

--- server/config.py
+++ server/config.py
@@ -1,7 +1,9 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -17,3 +19,8 @@ metadata = MetaData(naming_convention={
 })
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+CORS(app)
