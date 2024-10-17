523c65a67095c7e50ecec794c48771c260d56cb5
Collin Sakuma <collinmsakuma@gmail.com>
2024-10-17 10:39:15 -0700

    seed the routes table and the items table

--- server/seed.py
+++ server/seed.py
@@ -41,3 +41,23 @@ if __name__ == '__main__':
         operation_5 = Operation(
             operation_name = ''
         )
+
+        # seed the routes table 
+        route_1 = Route(
+            sequence = ''
+        )
+
+        # seed the items table
+        item_1 = Item(
+            item_name = 'endmill_1',
+            # route_id = '',
+            item_length = 50.8,
+            item_width = 12.7,
+            item_shank_length = 20,
+            has_flats = True,
+            has_neck = True,
+            item_neck_length = 5,
+            item_coating = 'C03',
+            has_edge_prep = True,
+            has_polishing = True
+        )
