8696cbfa74985a65e98f14ff5ba377cf645f398c
Collin Sakuma <collinmsakuma@gmail.com>
2024-10-17 10:07:41 -0700

    start seeding file, initial imports and clearing of old tables

--- server/seed.py
+++ server/seed.py
@@ -1,2 +1,15 @@
 from app import app
-from models import db, Machine, Operation, Route
+from models import db, Machine, Operation, Route, Item
+
+if __name__ == '__main__':
+
+    with app.app_context():
+        print('Starting seed...')
+        # clear previous tables before seeding
+
+        Machine.query.delete()
+        Operation.query.delete()
+        Route.query.delete()
+        Item.query.delete()
+
+        print('Seeding Finished...')
