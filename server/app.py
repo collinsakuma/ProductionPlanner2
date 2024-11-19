f873241bcb49cdb6135e08f075186e21b23c038f
Collin Sakuma <collinmsakuma@gmail.com>
2024-11-18 20:46:52 -0800

    run on port 5555

--- server/app.py
+++ server/app.py
@@ -9,6 +9,6 @@ from models import Machine, Operation, Route, Item
 @app.route('/<int:id>')
 def index(id=0):
     return render_template("index.html")
-
+# run on port ####
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
